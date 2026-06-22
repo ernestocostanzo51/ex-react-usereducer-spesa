792e16e0fa27a8dda06121eb782ad9f56aeaab51
ernestocostanzo51 <ernestanzo51@gmail.com>
2026-06-22 19:31:06 +0200

    milestone2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 
 
+
 function App() {
 const products = [
   { name: 'Mela', price: 0.5 },
@@ -8,6 +9,21 @@ const products = [
   { name: 'Latte', price: 1.0 },
   { name: 'Pasta', price: 0.7 },
 ]
+
+const [addedProducts , setaddedProducts] = useState([])
+
+const addToCart = (index) => {
+  const selectedProduct = 
+    products[index]
+
+const isAlreadyInCart = addedProducts.some(item => item.name === selectedProduct.name)
+
+    if (isAlreadyInCart) {
+      return 
+    }
+  setaddedProducts([...addedProducts , {...selectedProduct , quantity : 1}])
+ 
+}
 
   return (
     <>
@@ -17,9 +33,20 @@ const products = [
   products.map((product, index) => (
     <li key={index}>
       {product.name} <strong>{product.price}€</strong>
+      <button onClick={() => addToCart(index)}>Agg al carrello</button>
     </li>
   ))}
      </ul>
+
+     <h2>CARRELLO</h2>
+     <p>Ogetti il carrello : {addedProducts.length}</p>
+     <ul>
+          {addedProducts.map((item, index) => (
+            <li key={index}>
+              {item.name}  {item.price}€  x{item.quantity}
+            </li>
+          ))}
+        </ul>
      
     </>
   )
